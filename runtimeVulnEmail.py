ab17e574b020a56579268c45603712fa7187b074
Aaron Miles <132866139+aaronm-sysdig@users.noreply.github.com>
2023-09-22 22:34:49 +1000

    Update runtimeVulnEmail.py
    Changes to email logic to email the error screenshot if it can

--- runtimeVulnEmail.py
+++ runtimeVulnEmail.py
@@ -68,6 +68,48 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def email_screenshot(email_address, email_server_password, email_subject, email_body, obj_config, str_screenshot):
+    # Email the screenshot as an inline image
+    logging.info('Emailing enter()')
+    str_sender = obj_config['settings']['email']['from']
+    str_receiver = email_address
+    str_subject = email_subject
+    str_body = email_body
+
+    msg = MIMEMultipart('alternative')
+    msg['From'] = str_sender
+    msg['To'] = str_receiver
+    msg['Subject'] = str_subject
+
+    # Attach the plain text
+    msg.attach(MIMEText(str_body, 'plain'))
+
+    # Attach the PNG image inline
+    with open(str_screenshot, 'rb') as img:
+        mime_img = MIMEImage(img.read())
+        mime_img.add_header('Content-ID', '<screenshot>')
+        mime_img.add_header('Content-Disposition', 'inline', filename=str_screenshot)
+        msg.attach(mime_img)
+
+    # Attach HTML version referencing the inline image
+    html_body = f'{str_body}<br><img src="cid:screenshot">'
+    msg.attach(MIMEText(html_body, 'html'))
+
+    text = msg.as_string()
+
+    # Set up the SMTP server
+    server = smtplib.SMTP(obj_config['settings']['email']['server'], obj_config['settings']['email']['port'])
+    server.starttls()
+    server.login(obj_config['settings']['email']['username'], email_server_password)
+    server.sendmail(str_sender, str_receiver, text)
+    server.quit()
+    logging.info('Emailing exit()')
+
+    # Cleanup: Delete the screenshot
+    os.remove(str_screenshot)
+    logging.info('')
+
+
 def main():
     args = parse_arguments()
     with open(args.config, 'r') as file:
@@ -95,106 +137,85 @@ def main():
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
         if os.environ.get('CONTAINER') is not None:
+            logging.info('Found CONTAINER, running as container version')
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument(f"--window-size={obj_config['settings']['screen']['width']}x{obj_config['settings']['screen']['height']}")
         driver = webdriver.Chrome(options=chrome_options)
-
-        # Today
-        today = datetime.now().date()
-        # Yesterday
-        yesterday = today - timedelta(days=1)
-
-        # Midnight epoch
-        start_date = datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0)
-        # End of day
-        end_date = start_date + timedelta(days=1) - timedelta(seconds=1)
-
-        # Epoch as int
-        int_start_epoch = int(start_date.timestamp())
-        int_end_epoch = int(end_date.timestamp())
-
-        str_today_vuln_url = str_vuln_url + f"&from={int_start_epoch}&to={int_end_epoch}"
-
-        driver.implicitly_wait(10)
-        logging.info('Getting URL')
-        driver.get(str_today_vuln_url)
-        logging.info('Setting Username')
-        id_username = driver.find_element(by=By.NAME,
-                                          value='username')
-        id_username.click()
-        id_username.send_keys(str_username)
-
-        logging.info('Setting Password')
-        id_password = driver.find_element(by=By.NAME,
-                                          value='password')
-        id_password.click()
-        id_password.send_keys(str_password)
-
-        logging.info('Logging In')
-        id_submit = driver.find_element(by=By.XPATH,
-                                        value="//button[@type='submit']")
-        id_submit.click()
-
-        wait = WebDriverWait(driver=driver,
-                             timeout=driver.timeouts.implicit_wait)
-
-        driver.find_element(By.XPATH, '//div[@data-test="app-nav-collapse-btn"]').click()
-
-        # Wait for the data to be visible
-
         try:
-            for str_wait in item['waits'][0].values():
-                logging.info(f"Waiting for {str_wait}")
-                wait.until(EC.visibility_of_element_located(locator=(By.XPATH, str_wait)))
+            # Today
+            today = datetime.now().date()
+            # Yesterday
+            yesterday = today - timedelta(days=1)
+
+            # Midnight epoch
+            start_date = datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0)
+            # End of day
+            end_date = start_date + timedelta(days=1) - timedelta(seconds=1)
+
+            # Epoch as int
+            int_start_epoch = int(start_date.timestamp())
+            int_end_epoch = int(end_date.timestamp())
+
+            str_today_vuln_url = str_vuln_url + f"&from={int_start_epoch}&to={int_end_epoch}"
+
+            driver.implicitly_wait(10)
+            logging.info('Getting URL')
+            driver.get(str_today_vuln_url)
+            logging.info('Setting Username')
+            id_username = driver.find_element(by=By.NAME,
+                                              value='username')
+            id_username.click()
+            id_username.send_keys(str_username)
+
+            logging.info('Setting Password')
+            id_password = driver.find_element(by=By.NAME,
+                                              value='password')
+            id_password.click()
+            id_password.send_keys(str_password)
+
+            logging.info('Logging In')
+            id_submit = driver.find_element(by=By.XPATH,
+                                            value="//button[@type='submit']")
+            id_submit.click()
+
+            wait = WebDriverWait(driver=driver,
+                                 timeout=driver.timeouts.implicit_wait)
+
+            driver.find_element(By.XPATH, '//div[@data-test="app-nav-collapse-btn"]').click()
+
+            # Wait for the data to be visible
+
+            try:
+                for str_wait in item['waits'][0].values():
+                    logging.info(f"Waiting for {str_wait}")
+                    wait.until(EC.visibility_of_element_located(locator=(By.XPATH, str_wait)))
+            except Exception as e:
+                logging.error(f"An error occurred waiting for the display: {e}, continuing anyway")
+
+            str_screenshot = "RuntimeVulns.png"
+            logging.info('Taking screenshot')
+            driver.save_screenshot(str_screenshot)
+            logging.info('Cleaning up.  Running driver.quit()')
+            driver.quit()
         except Exception as e:
             logging.error(f"An error occurred waiting for the display: {e}, continuing anyway")
+            str_screenshot = "RuntimeVulns.png"
+            driver.save_screenshot(str_screenshot)
+            email_screenshot(email_address=item['email'],
+                             email_server_password=str_email_server_password,
+                             obj_config=obj_config,
+                             str_screenshot=str_screenshot,
+                             email_subject=obj_config['settings']['email']['subject'],
+                             email_body="Error Screenshot")
+            exit(1)
+        email_screenshot(email_address=item['email'],
+                         email_server_password=str_email_server_password,
+                         obj_config=obj_config,
+                         str_screenshot=str_screenshot,
+                         email_subject=obj_config['settings']['email']['subject'],
+                         email_body=obj_config['settings']['email']['body'])
 
-        str_screenshot = "RuntimeVulns.png"
-        logging.info('Taking screenshot')
-        driver.save_screenshot(str_screenshot)
-        logging.info('Cleaning up.  Running driver.quit()')
-        driver.quit()
-
-        # Email the screenshot as an inline image
-        logging.info('Emailing enter()')
-        str_sender = obj_config['settings']['email']['from']
-        str_receiver = item['email']
-        str_subject = obj_config['settings']['email']['subject']
-        str_body = obj_config['settings']['email']['body']
-
-        msg = MIMEMultipart('alternative')
-        msg['From'] = str_sender
-        msg['To'] = str_receiver
-        msg['Subject'] = str_subject
-
-        # Attach the plain text
-        msg.attach(MIMEText(str_body, 'plain'))
-
-        # Attach the PNG image inline
-        with open(str_screenshot, 'rb') as img:
-            mime_img = MIMEImage(img.read())
-            mime_img.add_header('Content-ID', '<screenshot>')
-            mime_img.add_header('Content-Disposition', 'inline', filename=str_screenshot)
-            msg.attach(mime_img)
-
-        # Attach HTML version referencing the inline image
-        html_body = f'{str_body}<br><img src="cid:screenshot">'
-        msg.attach(MIMEText(html_body, 'html'))
-
-        text = msg.as_string()
-
-        # Set up the SMTP server
-        server = smtplib.SMTP(obj_config['settings']['email']['server'], obj_config['settings']['email']['port'])
-        server.starttls()
-        server.login(obj_config['settings']['email']['username'], str_email_server_password)
-        server.sendmail(str_sender, str_receiver, text)
-        server.quit()
-        logging.info('Emailing exit()')
-
-        # Cleanup: Delete the screenshot
-        os.remove(str_screenshot)
-        logging.info('')
     logging.info('finished')
 
 
